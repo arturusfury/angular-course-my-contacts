e7cbbedb389d5ca3b0fb03de8ab0ed650045c57f
arturusfury <bri.reynolds@gmail.com>
2016-11-04 19:12:03 -0500

    added code for addFormSubmit

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -21,4 +21,47 @@ angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
   $scope.hide = function() {
     $scope.addFormShow = false;
   };
+
+  $scope.addFormSubmit = function() {
+    if($scope.name){ var name = $scope.name; } else { var name = null; }
+    if($scope.email){ var email = $scope.email; } else { var email = null; }
+    if($scope.company){ var company = $scope.company; } else { var company = null; }
+    if($scope.mobile_phone){ var mobile_phone = $scope.mobile_phone; } else { var mobile_phone = null; }
+    if($scope.home_phone){ var home_phone = $scope.home_phone; } else { var home_phone = null; }
+    if($scope.work_phone){ var work_phone = $scope.work_phone; } else { var work_phone = null; }
+    if($scope.street_address){ var street_address = $scope.street_address; } else { var street_address = null; }
+    if($scope.city){ var city = $scope.city; } else { var city = null; }
+    if($scope.state){ var state = $scope.state; } else { var state = null; }
+    if($scope.zipcode){ var zipcode = $scope.zipcode; } else { var zipcode = null; }
+
+    $scope.contacts.$add({
+      name: name,
+      email: email,
+      company: company,
+      phones: [
+        {
+          mobile_phone: mobile_phone,
+          home_phone: home_phone,
+          work_phone: work_phone
+        }
+      ],
+      address: [
+        {
+          street_address: street_address,
+          city: city,
+          state: state,
+          zipcode: zipcode
+        }
+      ]
+    }).then(function(ref) {
+      var id = ref.key();
+
+      console.log("Contact Added with ID: " + id)
+
+      clearFields();
+      $scope.addFormShow = false;
+
+      $scope.msg = "Contact Added";
+    });
+  }
 }]);
